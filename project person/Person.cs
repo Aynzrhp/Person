7a470f79d605365276edaece14b4f51de00783e3
Aynzrhp <aynazrahmniparhizkar@gmail.com>
2023-11-07 14:54:10 +0330

    Profile of people

---
+++ project person/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_person
+{
+    internal class Person
+    {
+        public string name;
+        public string family;
+        public string id;
+        public string website;
+
+
+        public Person(string name,string family,string id,string website)
+        {
+            this.name = name;
+            this.family = family;
+            this.id = id;
+            this.website = website;
+        }
+    }
+}
---
+++ project person/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Person[] people = new Person[number];
+
+            for(int i=0;i<number;i++)
+            {
+                Console.WriteLine($"Please enter person [{i + 1}] name:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"Please enter person [{i + 1}] family:");
+                string family = Console.ReadLine();
+
+
+                Console.WriteLine($"Please enter person [{i + 1}] id:");
+                string id = Console.ReadLine();
+
+                Console.WriteLine($"Please enter person [{i + 1}] website:");
+                string website = Console.ReadLine();
+
+                Person p = new Person(name, family, id, website);
+                people[i] = p;
+
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("***************************************************");
+            Console.ResetColor();
+             
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            foreach(Person person in people)
+            {
+                Console.WriteLine($"Name: {person.name}  Family: {person.family}  Id: {person.id}  Website: {person.website}");
+            }
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
